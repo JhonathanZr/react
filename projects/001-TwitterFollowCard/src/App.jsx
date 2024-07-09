4b039042f8e8c719a5e35f897411024cfdacb9a3
nhor <jhonathanzr@gmail.com>
2024-07-08 23:42:55 -0400

    finish 001

--- projects/001-TwitterFollowCard/src/App.jsx
+++ projects/001-TwitterFollowCard/src/App.jsx
@@ -1,12 +1,45 @@
 import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard.jsx'
+const users = [
+    {
+        userName: 'DevNhor',
+        name: 'Nhor',
+        initialIsFollowing: true
+    },
+    {
+        userName: 'BillGates',
+        name: 'Bill Gates',
+        initialIsFollowing: false
+    },
+    {
+        userName: 'NASA',
+        name: 'NASA',
+        initialIsFollowing: true
+    },
+    {
+        userName: 'midudev',
+        name: 'midudev',
+        initialIsFollowing: true
+    },
+]
 
 export function App () {
     return (
+        
         <section className='App'>
-            <TwitterFollowCard userName="DevNhor" name="Nhor" initialIsFollowing/>
-            <TwitterFollowCard userName="BillGates" name="Bill Gates" initialIsFollowing={false}/>
-            <TwitterFollowCard userName="NASA" name="NASA" initialIsFollowing={false}/>
+            <div className='tw-followCard-title'>
+                <h2>A quien seguir</h2>
+            </div>
+            {
+                users.map(({userName, name, initialIsFollowing}) => (
+                    <TwitterFollowCard
+                    key = {userName}
+                    userName={userName}
+                    initialIsFollowing={initialIsFollowing}
+                    >{name}
+                    </TwitterFollowCard>
+                ))
+        }
         </section>
     )
 }
--- projects/001-TwitterFollowCard/src/TwitterFollowCard.jsx
+++ projects/001-TwitterFollowCard/src/TwitterFollowCard.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 
 // eslint-disable-next-line react/prop-types, no-unused-vars
-export function TwitterFollowCard({userName = 'undefined', name, initialIsFollowing}){
+export function TwitterFollowCard({userName = 'undefined', children, initialIsFollowing}){
     const [isFollowing, setIsFollowing] = useState(initialIsFollowing)
 
     const text = isFollowing ? 'Siguiendo' : 'Seguir'
@@ -20,13 +20,16 @@ return(
         alt="El avatar de DevNhor" 
         src={`https://unavatar.io/${userName}`}></img>
         <div className='tw-followCard-info'>
-            <strong>{name}</strong>
+            <strong>{children}</strong>
             <span className='tw-followCard-infoUserName'>@{userName}</span>
         </div>
     </header>
     <aside>
-        <button className={buttonClassName} onClick={handleClick}>{text}</button>
+        <button className={buttonClassName} onClick={handleClick}>
+        <span className='tw-followCard-text'>{text}</span>
         <span className='tw-followCard-stopFollow'>Dejar de seguir</span>
+        </button>
+        
     </aside>
 </article>
 )
